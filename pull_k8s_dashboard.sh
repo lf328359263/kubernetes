bade8528c4ace9f146e566de19d870ff7642fa0f
root <kxsh2015@163.com>
2018-11-21 08:20:59 -0500

    dashboard images

--- pull_k8s_dashboard.sh
+++ pull_k8s_dashboard.sh
@@ -1,38 +1,9 @@
 #!/bin/bash
-MY_REGISTRY=registry.cn-hangzhou.aliyuncs.com/kongxiangshuai
-K8S_DASHBOARD_VERSION=v1.12.2
+#拉取镜像
+docker pull registry.cn-qingdao.aliyuncs.com/wangxiaoke/kubernetes-dashboard-amd64:v1.10.0
 
-echo ""
-echo "=========================================================="
-echo "Pull Kubernetes DashBoard ${K8S_DASHBOARD_VERSION} Images from aliyuncs.com ......"
-echo "=========================================================="
-echo ""
+#重新打标签
+docker tag registry.cn-qingdao.aliyuncs.com/wangxiaoke/kubernetes-dashboard-amd64:v1.10.0 k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.0
 
-## 拉取镜像
-docker pull ${MY_REGISTRY}/:${COREDNS_VERSION}
-
-
-## 添加Tag
-docker tag ${MY_REGISTRY}/kube-apiserver:${K8S_VERSION} k8s.gcr.io/kube-apiserver:${K8S_VERSION}
-docker tag ${MY_REGISTRY}/kube-scheduler:${K8S_VERSION} k8s.gcr.io/kube-scheduler:${K8S_VERSION}
-docker tag ${MY_REGISTRY}/kube-controller-manager:${K8S_VERSION} k8s.gcr.io/kube-controller-manager:${K8S_VERSION}
-docker tag ${MY_REGISTRY}/kube-proxy:${K8S_VERSION} k8s.gcr.io/kube-proxy:${K8S_VERSION}
-docker tag ${MY_REGISTRY}/etcd:${ETC_VERSION} k8s.gcr.io/etcd:${ETC_VERSION}
-docker tag ${MY_REGISTRY}/pause:${PAUSE_VERSION} k8s.gcr.io/pause:${PAUSE_VERSION}
-docker tag ${MY_REGISTRY}/coredns:${COREDNS_VERSION} k8s.gcr.io/coredns:${COREDNS_VERSION}
-
-## 
-docker rmi ${MY_REGISTRY}/kube-apiserver:${K8S_VERSION}
-docker rmi ${MY_REGISTRY}/kube-scheduler:${K8S_VERSION}
-docker rmi ${MY_REGISTRY}/kube-controller-manager:${K8S_VERSION}
-docker rmi ${MY_REGISTRY}/kube-proxy:${K8S_VERSION}
-docker rmi ${MY_REGISTRY}/etcd:${ETC_VERSION}
-docker rmi ${MY_REGISTRY}/pause:${PAUSE_VERSION}
-docker rmi ${MY_REGISTRY}/coredns:${COREDNS_VERSION}
-
-echo ""
-echo "=========================================================="
-echo "Pull Kubernetes ${K8S_VERSION} Images FINISHED."
-echo "=========================================================="
-
-echo ""
+#删除无用镜像
+docker image rm registry.cn-qingdao.aliyuncs.com/wangxiaoke/kubernetes-dashboard-amd64:v1.10.0
